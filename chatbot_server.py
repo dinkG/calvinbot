51c011442a9609686fac2396cfcc2fc47e2cd906
dinkG <dinkgaming1@gmail.com>
2024-09-16 09:02:57 -0500

    Update CORS settings to allow requests from http://theologiananswers.com

--- chatbot_server.py
+++ chatbot_server.py
@@ -4,8 +4,9 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-# Enable CORS for all origins
-CORS(app)
+
+# Enable CORS for the specified origin
+CORS(app, resources={r"/chat": {"origins": "http://theologiananswers.com"}})
 
 # Define the API URL from an environment variable or use a default one
 API_URL = os.getenv('API_URL', 'https://lnuv0i4a09.execute-api.us-east-1.amazonaws.com/dev/')
